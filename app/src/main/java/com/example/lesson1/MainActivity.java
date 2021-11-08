6b6234e3be57ebdf0ff80315e3a407773131be95
Vladimir_Yurevich <dvagig@gmail.com>
2021-11-08 21:29:15 +0300

    Реализовали действие на нажатие кнопки

--- app/src/main/java/com/example/lesson1/MainActivity.java
+++ app/src/main/java/com/example/lesson1/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.lesson1;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+private Button button;
+private TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=findViewById(R.id.run);
+        textView=findViewById(R.id.tex_v);
+        View.OnClickListener onClickListener=new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Нажали кнопку!");
+            }
+        };
+        button.setOnClickListener(onClickListener);
+
     }
 }
